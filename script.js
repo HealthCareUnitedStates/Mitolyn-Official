29e3bfcea2c5f3ea5b38cd0f58ae5c7a32fe44cb
Mitolyn <mitolynsales@gmail.com>
2025-04-02 08:22:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  // Disable Right Click
  document.oncontextmenu = () => {
-    alert("Hahaha...😂__DEVELOPER IS KISHAN__😎")
+    alert("Love curiosity! But for official info, check out our website.")
     return false;
 }
 
@@ -15,7 +15,7 @@ document.addEventListener("keydown", function (e) {
         e.key === "F12"
     ) {
         e.preventDefault();
-        alert("Not again.....!😂");
+        alert("Love curiosity! But for official info, check out our website.");
     }
 });
 
